6faf385b80e80f5be38af8ec1edb836d294dc64c
Jaderson Aires <jadersonaires@gmail.com>
2023-01-17 18:05:49 -0300

    Primeira aula do Setup

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+import { Habit } from "./components/Habit";
+
+export function App() {
+  return (
+    <>
+      <Habit completed={2} />
+      <Habit completed={3} />
+      <Habit completed={1} />
+    </>
+  )
+}
